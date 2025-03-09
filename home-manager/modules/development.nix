d442fcc50683d7f0a07d0ba24c12590f7495ce0f
Mika Drakolis <mika.drakolis@pm.me>
2025-03-09 17:24:45 +0100

    Add direnv

--- home-manager/modules/development.nix
+++ home-manager/modules/development.nix
@@ -36,32 +36,42 @@ in {
     terraform-ls
   ];
 
-  programs.gitui = {
-    enable = true;
-    theme = ''
-      (
-        selected_tab: Some("Reset"),
-        command_fg: Some("#${style.colors.textInverted}"),
-        selection_bg: Some("#${style.colors.selection}"),
-        selection_fg: Some("#${style.colors.textInverted}"),
-        cmdbar_bg: Some("#${style.colors.primary}"),
-        cmdbar_extra_lines_bg: Some("#${style.colors.primary}"),
-        disabled_fg: Some("#${style.colors.textGray}"),
-        diff_line_add: Some("#${style.colors.gitAdded}"),
-        diff_line_delete: Some("#${style.colors.gitRemoved}"),
-        diff_file_added: Some("#${style.colors.gitAdded}"),
-        diff_file_removed: Some("#${style.colors.gitRemoved}"),
-        diff_file_moved: Some("#${style.colors.gitMoved}"),
-        diff_file_modified: Some("#${style.colors.gitModified}"),
-        commit_hash: Some("#${style.colors.gitCommit}"),
-        commit_time: Some("#${style.colors.gitTime}"),
-        commit_author: Some("#${style.colors.gitAuthor}"),
-        danger_fg: Some("#${style.colors.error}"),
-        push_gauge_bg: Some("#${style.colors.primary}"),
-        push_gauge_fg: Some("#${style.colors.textInverted}"),
-        tag_fg: Some("#${style.colors.gitTag}"),
-        branch_fg: Some("#${style.colors.gitBranch}"),
-      )
-    '';
+  programs = {
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    gitui = {
+      enable = true;
+      theme = ''
+        (
+          selected_tab: Some("Reset"),
+          command_fg: Some("#${style.colors.textInverted}"),
+          selection_bg: Some("#${style.colors.selection}"),
+          selection_fg: Some("#${style.colors.textInverted}"),
+          cmdbar_bg: Some("#${style.colors.primary}"),
+          cmdbar_extra_lines_bg: Some("#${style.colors.primary}"),
+          disabled_fg: Some("#${style.colors.textGray}"),
+          diff_line_add: Some("#${style.colors.gitAdded}"),
+          diff_line_delete: Some("#${style.colors.gitRemoved}"),
+          diff_file_added: Some("#${style.colors.gitAdded}"),
+          diff_file_removed: Some("#${style.colors.gitRemoved}"),
+          diff_file_moved: Some("#${style.colors.gitMoved}"),
+          diff_file_modified: Some("#${style.colors.gitModified}"),
+          commit_hash: Some("#${style.colors.gitCommit}"),
+          commit_time: Some("#${style.colors.gitTime}"),
+          commit_author: Some("#${style.colors.gitAuthor}"),
+          danger_fg: Some("#${style.colors.error}"),
+          push_gauge_bg: Some("#${style.colors.primary}"),
+          push_gauge_fg: Some("#${style.colors.textInverted}"),
+          tag_fg: Some("#${style.colors.gitTag}"),
+          branch_fg: Some("#${style.colors.gitBranch}"),
+        )
+      '';
+    };
   };
 }
