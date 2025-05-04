cda272b0c20b456cd44025fcf33a2422039ef19d
Mika Drakolis <mika.drakolis@pm.me>
2025-05-04 19:05:29 +0200

    Add arguments section

--- home-manager/hosts/WorkerBee.nix
+++ home-manager/hosts/WorkerBee.nix
@@ -2,7 +2,6 @@
 {
   imports = [
     ../modules
-    ../modules/desktop/kde
   ];
 
   # Nicely reload system units when changing configs
@@ -10,6 +9,4 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.11";
-
-  home.keyboard = null;
 }
