04f6c5b53a4ddc23d53111e25d3a48a8636574b3
Mika Drakolis <mika.drakolis@pm.me>
2025-05-05 07:22:00 +0200

    Remove trayapp since its not needed on KDE

--- home-manager/modules/sync.nix
+++ home-manager/modules/sync.nix
@@ -3,12 +3,10 @@
   home.packages = with pkgs; [
     vdirsyncer
     rclone
-    syncthingtray-qt6
   ];
 
   services.syncthing = {
     enable = true;
-    # tray.enable = true;
     settings = {
       gui = {
         user = "drakolis";
