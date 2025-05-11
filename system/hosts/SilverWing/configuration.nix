a15b19f77a60f2668c730fe05be4d5dd879c369c
Mika Zimina <mika.zimina@bonial.com>
2025-05-11 14:50:45 +0200

    Add SilverWing config

---
+++ system/hosts/SilverWing/configuration.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+{
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    vim
+    home-manager
+  ];
+
+  # Enable alternative shell support in nix-darwin.
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+}
