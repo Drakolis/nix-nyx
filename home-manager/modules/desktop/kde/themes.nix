a3cac8966935d2efea583c7264c9a4064b446537
Mika Drakolis <mika.drakolis@pm.me>
2025-05-31 10:29:19 +0200

    Add theme for profiles

--- home-manager/modules/desktop/kde/themes.nix
+++ home-manager/modules/desktop/kde/themes.nix
@@ -29,6 +29,24 @@ let
       plasma-apply-colorscheme "$new_theme"
     fi
   '';
+  theme-switch-fusion = pkgs.writeShellScriptBin "theme-switch-fusion" ''
+    plasma-apply-colorscheme "CatppuccinMochaMauve"
+  '';
+  theme-switch-hacker = pkgs.writeShellScriptBin "theme-switch-hacker" ''
+    plasma-apply-colorscheme "CatppuccinMochaBlue"
+  '';
+  theme-switch-creative = pkgs.writeShellScriptBin "theme-switch-creative" ''
+    plasma-apply-colorscheme "CatppuccinMochaPink"
+  '';
+  theme-switch-engineer = pkgs.writeShellScriptBin "theme-switch-engineer" ''
+    plasma-apply-colorscheme "CatppuccinMochaSapphire"
+  '';
+  theme-switch-adventurer = pkgs.writeShellScriptBin "theme-switch-adventurer" ''
+    plasma-apply-colorscheme "CatppuccinMochaGreen"
+  '';
+  theme-switch-business = pkgs.writeShellScriptBin "theme-switch-business" ''
+    plasma-apply-colorscheme "CatppuccinMochaRosewater"
+  '';
 in
 {
   home.packages = with pkgs; [
@@ -37,7 +55,47 @@ in
       flavour = [ "mocha" ];
     })
     (catppuccin-kde.override {
+      accents = [ "blue" ];
+      flavour = [ "mocha" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "pink" ];
+      flavour = [ "mocha" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "sapphire" ];
+      flavour = [ "mocha" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "green" ];
+      flavour = [ "mocha" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "rosewater" ];
+      flavour = [ "mocha" ];
+    })
+    (catppuccin-kde.override {
       accents = [ "mauve" ];
+      flavour = [ "latte" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "blue" ];
+      flavour = [ "latte" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "pink" ];
+      flavour = [ "latte" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "sapphire" ];
+      flavour = [ "latte" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "green" ];
+      flavour = [ "latte" ];
+    })
+    (catppuccin-kde.override {
+      accents = [ "rosewater" ];
       flavour = [ "latte" ];
     })
     catppuccinifier-cli
@@ -46,6 +104,12 @@ in
     kdePackages.kconfig
 
     theme-switcher
+    theme-switch-fusion
+    theme-switch-hacker
+    theme-switch-creative
+    theme-switch-engineer
+    theme-switch-adventurer
+    theme-switch-business
   ];
 
   # Systemd user service
