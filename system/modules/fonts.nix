027307bc54a959009b16fe7a5964075a0d8785fa
Mika Drakolis <mika.drakolis@pm.me>
2025-05-31 09:03:41 +0200

    Refactor fonts

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -4,49 +4,23 @@
   lib,
   ...
 }:
-let
-  cfg = config.drakolis.fonts;
-  fontSets = lib.types.enum [
-    "default"
-    "ubuntu"
-  ];
-in
-with lib;
 {
-  options = {
-    drakolis.fonts = {
-      fontSet = mkOption {
-        default = "default";
-        type = fontSets;
-        description = ''
-          Select the font set to use.
-        '';
-      };
-    };
-  };
-  config = {
-    fonts.packages =
-      with pkgs;
-      if (cfg.fontSet == "ubuntu") then
-        [
-          ubuntu-sans
-          ubuntu-sans-mono
-          nerd-fonts.ubuntu
-          nerd-fonts.ubuntu-mono
-        ]
-        ++ lib.optionals stdenv.hostPlatform.isLinux [
-          noto-fonts-emoji
-        ]
-      else
-        [
-          noto-fonts
-          noto-fonts-extra
-          noto-fonts-cjk-sans
-          hack-font
-          nerd-fonts.hack
-        ]
-        ++ lib.optionals stdenv.hostPlatform.isLinux [
-          noto-fonts-emoji
-        ];
-  };
+  fonts.packages =
+    with pkgs;
+    [
+      ubuntu-sans
+      ubuntu-sans-mono
+      nerd-fonts.ubuntu
+      nerd-fonts.ubuntu-mono
+
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-cjk-sans
+
+      hack-font
+      nerd-fonts.hack
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      noto-fonts-emoji
+    ];
 }
