97def74248c198fb64516910cfd1692b1941d325
Mika Drakolis <mika.drakolis@pm.me>
2025-10-25 16:59:09 +0200

    Remove argumant that doesnt exist

--- system/modules/nix/nix.nix
+++ system/modules/nix/nix.nix
@@ -19,7 +19,6 @@
       enable = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
-      enableNushellIntegration = true;
       nix-direnv.enable = true;
     };
   };
