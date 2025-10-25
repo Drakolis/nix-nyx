43004fa01c9358447c77616fce668a3e99e1a6c2
Mika Drakolis <mika.drakolis@pm.me>
2025-10-25 11:18:41 +0200

    Add tailscale tray

--- system/modules/tailscale.nix
+++ system/modules/tailscale.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ tailscale ];
+  environment.systemPackages =
+    with pkgs;
+    [ tailscale ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      tailscale-systray
+    ];
 
   services.tailscale.enable = true;
   services.tailscale.authKeyFile = "/run/secrets/tailscale_key";
