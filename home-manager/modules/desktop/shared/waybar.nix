e9b3de10d2b5b409b62534f4efe3153e3b8cf079
Mika Drakolis <mika.drakolis@pm.me>
2025-10-31 22:29:57 +0100

    Small adjustments to waybar paddings

--- home-manager/modules/desktop/shared/waybar.nix
+++ home-manager/modules/desktop/shared/waybar.nix
@@ -346,21 +346,21 @@ in
       /* Section - Left */
       #language, #custom-lock, #custom-notification {
         border-radius: 2rem 0 0 2rem;
-        padding-right: 0.3rem;
+        padding-right: 0.35rem;
         margin-left: 5px;
       }
 
       /* Section - Middle */
       #pulseaudio, #wireplumber, #backlight, #battery,
       #bluetooth, #clock {
-        padding-left: 0.3rem;
-        padding-right: 0.3rem;
+        padding-left: 0.35rem;
+        padding-right: 0.35rem;
       }
 
       /* Section - Right */
       #custom-power, #custom-music, #network, #custom-weather {
         border-radius: 0 2rem 2rem 0;
-        padding-left: 0.3rem;
+        padding-left: 0.35rem;
         margin-right: 5px;
       }
 
