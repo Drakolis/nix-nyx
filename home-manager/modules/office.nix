9af1f13f3fa83fefad805354134ceeac6877d613
Mika Drakolis <mika.drakolis@pm.me>
2025-11-18 21:06:57 +0100

    Add okular

--- home-manager/modules/office.nix
+++ home-manager/modules/office.nix
@@ -1,18 +1,18 @@
 { pkgs, lib, ... }:
 let
-  desiredOfficePackages =
-    [
-      pkgs.obsidian
-      (pkgs.writeShellScriptBin "wttr" ''
-        curl wttr.in/$1
-      '')
-    ]
-    ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-    ]
-    ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-      pkgs.maccy
-      pkgs.numi
-    ];
+  desiredOfficePackages = [
+    pkgs.obsidian
+    (pkgs.writeShellScriptBin "wttr" ''
+      curl wttr.in/$1
+    '')
+  ]
+  ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+    pkgs.kdePackages.okular
+  ]
+  ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+    pkgs.maccy
+    pkgs.numi
+  ];
 in
 {
   home.packages = desiredOfficePackages;
