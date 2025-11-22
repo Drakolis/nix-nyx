dd298777becf1935c4a4bfbd0992702de112ca81
Mika Drakolis <mika.drakolis@pm.me>
2025-11-22 14:42:15 +0100

    Add Joermungandr to sync

--- home-manager/modules/sync.nix
+++ home-manager/modules/sync.nix
@@ -26,6 +26,9 @@ with lib;
         "mb-H02L4YFQ6P" = {
           id = "ZZKPN74-4BCOU4T-6K4IXL3-IKL4DDF-7B3JIDX-2LWZWGU-OPYYJJS-SEDALAZ";
         };
+        "Joermungandr" = {
+          id = "FLWAKCR-RDRCUZD-4JLCEX4-T2FJVNI-VVFAW33-3NEYJKT-S3DKMU7-NSI44A3";
+        };
       };
       folders = {
         "Drakolis Files" = {
@@ -35,6 +38,7 @@ with lib;
             "WinterDragon"
             "SilverWing"
             "mb-H02L4YFQ6P"
+            "Joermungandr"
           ];
         };
       };
