75c351a3d8533ca7633e36f65e423c753623b2fa
Mika Drakolis <mika.drakolis@pm.me>
2025-11-30 11:38:36 +0100

    Add Jo SSH key

--- system/users/drakolis.nix
+++ system/users/drakolis.nix
@@ -33,11 +33,12 @@ with lib;
     ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIT5ng625FZcgq7XizZGVN2PyJkDAY9QVm8Frk8XCmx/ drakolis@Joermungandr"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILrwMCLeZhbuep6a0BHGJe3DLY5KzVQ6ylwvd1Rjv/T/ drakolis@SilverWing"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpvdctXVRoijDWhYa4BTX33+JfFE0a3+9+tkaB4Szup drakolis@YDdraigGoch"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLXFwVqF0t+J5J1pN4To05Ddtmyd7DBbvHTdoXAC8fw drakolis@NanoWyrm"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJaNfEcfSnRGQF4k4PWtFhPEME2Ao9QWTyhu/w+/ftgb drakolis@WinterDragon"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUpNcUS4NNyE+yDz/npwhZojZcH17Hybtc95aWbkA4i" # WorkerBee
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpvdctXVRoijDWhYa4BTX33+JfFE0a3+9+tkaB4Szup drakolis@YDdraigGoch"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLXFwVqF0t+J5J1pN4To05Ddtmyd7DBbvHTdoXAC8fw drakolis@NanoWyrm"
     ];
   };
 }
