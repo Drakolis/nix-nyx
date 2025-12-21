7b5a8bd2666caf23d929dada07f7c6186aa8bc77
Mika Drakolis <mika.drakolis@pm.me>
2025-12-21 20:13:37 +0100

    Fixing theming issues

--- home-manager/modules/desktop/shared/themes.nix
+++ home-manager/modules/desktop/shared/themes.nix
@@ -8,10 +8,10 @@ in
     #   accents = [ "mauve" ];
     #   flavour = [ "mocha" ];
     # })
-    # catppuccinifier-cli
-    # catppuccin-whiskers
+    catppuccinifier-cli
+    catppuccin-whiskers
 
-    # # Other DEs
+    # Other DEs
     catppuccin-qt5ct
     kdePackages.breeze
     kdePackages.breeze-gtk
@@ -19,12 +19,16 @@ in
     vanilla-dmz
     catppuccin-papirus-folders
 
-    # # Kvantum support
-    # qt6Packages.qtstyleplugin-kvantum
-    # (catppuccin-kvantum.override {
-    #   accent = "mauve";
-    #   variant = "mocha";
-    # })
+    qt6Packages.qt6ct
+    libsForQt5.qt5ct
+    nwg-look
+
+    # Kvantum support
+    qt6Packages.qtstyleplugin-kvantum
+    (catppuccin-kvantum.override {
+      accent = "mauve";
+      variant = "mocha";
+    })
 
     # # GTK (Deprecated)
     (catppuccin-gtk.override {
@@ -33,47 +37,48 @@ in
     })
   ];
 
-  # home.pointerCursor = {
-  #   package = pkgs.vanilla-dmz; # For some reason this is mandatory to repeat here
-  #   name = "DMZ-Black";
-  #   size = style.cursorTheme.size;
-  #   hyprcursor.enable = true;
-  #   gtk.enable = true;
-  #   x11.enable = true;
-  # };
+  home.pointerCursor = {
+    package = pkgs.vanilla-dmz; # For some reason this is mandatory to repeat here
+    name = "DMZ-Black";
+    size = style.cursorTheme.size;
+    #   hyprcursor.enable = true;
+    gtk.enable = true;
+    #   x11.enable = true;
+  };
 
   gtk = {
     enable = true;
     colorScheme = "dark";
-    # cursorTheme = {
-    #   name = "DMZ-Black";
-    #   size = style.cursorTheme.size;
-    # };
+    cursorTheme = {
+      name = "DMZ-Black";
+      size = style.cursorTheme.size;
+    };
     font = {
       name = style.fontGui;
       size = style.fontSize;
     };
     iconTheme = {
-      name = style.iconTheme.name;
+      name = "Papirus-Dark";
     };
     theme = {
-      # name = "catppuccin-mocha-mauve-standard";
-      name = "Breeze";
+      name = "catppuccin-mocha-mauve-standard";
+      # name = "Breeze";
     };
     gtk3.extraConfig = {
       gtk-menu-images = 1;
-      gtk-button-images = 0;
+      gtk-button-images = 1;
     };
   };
 
   qt = {
     enable = true;
     platformTheme = {
-      name = "kde";
+      name = "qtct";
     };
     style = {
       name = "breeze";
     };
+
   };
 
   # home.file = {
--- home-manager/modules/desktop/shared/xdg.nix
+++ home-manager/modules/desktop/shared/xdg.nix
@@ -3,6 +3,7 @@
   home.packages = [
     pkgs.kdePackages.xdg-desktop-portal-kde
   ];
+
   xdg.terminal-exec = {
     enable = true;
     settings = {
@@ -14,19 +15,24 @@
       ];
     };
   };
+
   xdg.portal = {
     enable = true;
     config = {
       common = {
         default = [
           "kde"
+          "gnome"
           "gtk"
         ];
         "org.freedesktop.impl.portal.Secret" = [ "kwallet" ];
+        "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
       };
       kde = {
         default = [
           "kde"
+          "gnome"
           "gtk"
         ];
         "org.freedesktop.impl.portal.Secret" = [ "kwallet" ];
