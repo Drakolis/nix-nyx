e12daef2a642f5a5aa9c4cdcd01e9eb610b7d48b
Mika Drakolis <mika.drakolis@pm.me>
2026-01-19 19:17:38 +0100

    Notifications copypaste cleanup

--- home-manager/modules/desktop/shared/files/ignis/notifications/notifications.py
+++ home-manager/modules/desktop/shared/files/ignis/notifications/notifications.py
@@ -21,7 +21,7 @@ class ScreenshotLayout(widgets.Box):
               content_fit="cover",
               width=1920 // 7,
               height=1080 // 7,
-              style="border-radius: 1rem; background-color: black;",
+              css_classes=["notification-screenshot"],
             ),
             widgets.Button(
               child=widgets.Icon(image="window-close-symbolic", pixel_size=20),
@@ -73,6 +73,7 @@ class NormalLayout(widgets.Box):
         widgets.Box(
           child=[
             widgets.Icon(
+              css_classes=["notification-icon"],
               image=notification.icon
               if notification.icon
               else "dialog-information-symbolic",
@@ -82,7 +83,6 @@ class NormalLayout(widgets.Box):
             ),
             widgets.Box(
               vertical=True,
-              style="margin-left: 0.75rem;",
               child=[
                 widgets.Label(
                   ellipsize="end",
@@ -93,15 +93,17 @@ class NormalLayout(widgets.Box):
                 ),
                 widgets.Label(
                   label=notification.body,
-                  ellipsize="end",
                   halign="start",
                   css_classes=["notification-body"],
                   visible=notification.body != "",
+                  wrap=True,
+                  wrap_mode="word",
+                  ellipsize="end",
                 ),
               ],
             ),
             widgets.Button(
-              child=widgets.Icon(image="window-close-symbolic", pixel_size=20),
+              child=widgets.Icon(image="window-close-symbolic", pixel_size=24),
               halign="end",
               valign="start",
               hexpand=True,
@@ -150,9 +152,10 @@ class Popup(widgets.Box):
     self._window = window
 
     widget = NotificationWidget(notification)
-    widget.css_classes = ["notification-popup", "popup"]
-    self._outer = widgets.Revealer(transition_type="slide_down", child=widget)
-    super().__init__(child=[self._outer], halign="center")
+    widget.css_classes = ["notification-popup", "elevation4"]
+    self._inner = widgets.Revealer(transition_type="slide_left", child=widget)
+    self._outer = widgets.Revealer(transition_type="slide_down", child=self._inner)
+    super().__init__(child=[self._outer], halign="end")
 
     notification.connect("dismissed", lambda x: self.destroy())
 
@@ -166,8 +169,8 @@ class Popup(widgets.Box):
       self._outer.reveal_child = False
       utils.Timeout(self._outer.transition_duration, box_destroy)
 
-    self._outer.transition_type = "crossfade"
-    self._outer.reveal_child = False
+    self._inner.transition_type = "crossfade"
+    self._inner.reveal_child = False
     utils.Timeout(self._outer.transition_duration, outer_close)
 
 
@@ -189,18 +192,19 @@ class PopupBox(widgets.Box):
     self._window.visible = True
     popup = Popup(box=self, window=self._window, notification=notification)
     self.prepend(popup)
-    utils.Timeout(popup._outer.transition_duration, popup._outer.set_reveal_child, True)
+    popup._outer.reveal_child = True
+    utils.Timeout(popup._outer.transition_duration, popup._inner.set_reveal_child, True)
 
 
 class NotificationPopup(widgets.Window):
   def __init__(self, monitor: int):
     super().__init__(
-      anchor=["right", "top", "left"],
+      anchor=["right", "top", "bottom"],
       monitor=monitor,
       namespace=f"ignis_notification_popup_{monitor}",
       layer="top",
       child=PopupBox(window=self, monitor=monitor),
       visible=False,
       dynamic_input_region=True,
-      css_classes=["rec-unset"],
+      css_classes=["unset-window", "notifications-window"],
     )
