ba73e637876259fcfc315eb88fb22f5b85953da7
Mika Drakolis <mika.drakolis@pm.me>
2026-03-10 20:51:32 +0100

    Add qutebrowser

--- home-manager/modules/browser.nix
+++ home-manager/modules/browser.nix
@@ -291,6 +291,19 @@ in
 {
   nixpkgs.config.librewolf.enablePlasmaBrowserIntegration = pkgs.stdenv.hostPlatform.isLinux;
 
+  programs.qutebrowser = {
+    enable = true;
+    extraConfig = ''
+      import catppuccin
+      # load your autoconfig, use this, if the rest of your config is empty!
+      config.load_autoconfig()
+      # set the flavor you'd like to use
+      # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+      # last argument (optional, default is False): enable the plain look for the menu rows
+      catppuccin.setup(c, "mocha", True)
+    '';
+  };
+
   programs.librewolf = {
     enable = false;
     nativeMessagingHosts = nativeMessagingHostsForPlatform;
@@ -312,6 +325,7 @@ in
   programs.zen-browser = {
     enable = true;
     nativeMessagingHosts = nativeMessagingHostsForPlatform;
+    suppressXdgMigrationWarning = true;
 
     policies = firefoxPolicies;
 
