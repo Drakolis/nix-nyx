0539bb21994679dbaee6d3d327d65d0ae23cfbc3
Mika Drakolis <mika.drakolis@pm.me>
2026-03-12 13:44:55 +0100

    Improve eggshell currency module

--- static/ignis/launcher/modes/currency.py
+++ static/ignis/launcher/modes/currency.py
@@ -6,7 +6,7 @@ from ._base import LauncherActionDefinition, LauncherModeBase
 API_URL = "https://open.er-api.com/v6/latest/USD"
 
 
-def fetch_rates():
+def fetch_rates():  # TODO: Should be cached every hour
   response = requests.get(API_URL)
   response.raise_for_status()
   data = response.json()
@@ -14,24 +14,42 @@ def fetch_rates():
   return rates
 
 
-def map_currency_to_action(number, target_currency):
+def convert_int(s, d):
+  try:
+    i = int(s)
+  except ValueError:
+    i = d
+  return i
+
+
+def map_currency_to_action(
+  value_source, currency_source, value_target, currency_target
+):
+  display = (
+    f"{value_source:.2f} {currency_source} is {value_target:.2f} {currency_target}"
+  )
   return LauncherActionDefinition(
-    title=f"{number}, {target_currency}",
+    title=display,
     icon="emblem-synchronizing",
-    action=lambda: utils.exec_sh(f'wl-copy "{number}"'),
+    action=lambda: utils.exec_sh(f'wl-copy "{value_target}"'),
   )
 
 
 def currency_search(query) -> [LauncherActionDefinition]:
   rates = fetch_rates()
-  brl_per_usd = rates["BRL"], "BRL"
-  eur_per_usd = rates["EUR"], "EUR"
-  rub_per_usd = rates["RUB"], "RUB"
 
-  return [
-    map_currency_to_action(value[0], value[1])
-    for value in [brl_per_usd, eur_per_usd, rub_per_usd]
-  ]
+  default_value = 1
+  default_source = "EUR"
+  default_target = "RUB"
+
+  split_query = query.split(",")
+
+  value = convert_int(split_query[0], default_value)
+  source = split_query[1].strip() if len(split_query) > 1 else default_source
+  target = split_query[2].strip() if len(split_query) > 2 else default_target
+  result = round((value / rates[source]) * rates[target], 2)
+
+  return [map_currency_to_action(value, source, result, target)]
 
 
 class LauncherModeCurrency(LauncherModeBase):
